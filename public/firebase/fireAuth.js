9bbf6589c081ff6990583eb6f76dbe4121dfa984
odafeaggreh <aggrehodafe2020@outlook.com>
2021-12-30 11:18:46 -0800

    finalized dashboard auth

--- public/firebase/fireAuth.js
+++ public/firebase/fireAuth.js
@@ -1,9 +1,11 @@
+console.log(44444444444444444);
 auth.onAuthStateChanged(function (user) {
   if (user) {
+    console.log("user is signed in");
     // User is signed in.
-    // setUpDashboard(user);
-    // userUpdate(user);
-    // userAddress(user);
+    setUpDashboard(user);
+    userUpdate(user);
+    userAddress(user);
   } else {
     // User is signed out.
     console.log("User is signed out");
---
+++ public/firebase/index.js
@@ -0,0 +1,65 @@
+const userFullName = document.querySelector("#userName");
+const userAccountBalance = document.querySelector("#userAccountBalance");
+
+const setUpDashboard = (user) => {
+  db.collection("users")
+    .doc(user.uid)
+    .onSnapshot((details) => {
+      userFullName.innerHTML = `
+            
+
+        <div class="welcome-msg pt-3 pb-4">
+        <h1 class='text-white' >Hi <span class="text-warning"> ${
+          details.data().fullName
+        } </span>, Welcome back</h1>
+        <div class='hide-md mt-4'>    
+          <div class='alert alert-info alert-dismissible fade show' role='alert'>
+          </div>    
+        </div>
+        </div>
+          `;
+
+      const totalDepositNum = details
+        .data()
+        .totalDeposit.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+
+      const totalBalanceNum = details
+        .data()
+        .totalBalance.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+
+      userAccountBalance.innerHTML = `
+          
+
+        <div class="statistics hide-md">
+        <div class="row">
+          <div class="col-xl-6 pr-xl-2">
+            <div class="row">
+              <div class="col-sm-6 pr-sm-2 statistics-grid">
+                <div class="card card_border border-primary-top p-4">
+                  <i class="fa fa-money"> </i>
+                  <h3 class="text-success number">${totalDepositNum}</h3>
+                  <p class="stat-text"> Account Balance </p>
+                </div>
+              </div>
+              <div class="col-sm-6 pl-sm-2 statistics-grid">
+                <div class="card card_border border-primary-top p-4">
+                  <i class="fa fa-money"> </i>
+                  <h3 class="text-secondary number">
+                  ${totalBalanceNum}  
+                </h3>
+                  <p class="stat-text">Total Deposit</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+          `;
+    });
+};
